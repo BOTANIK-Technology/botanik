235c4ca99f0359d2f9f056acecf29cd802aa6635
p-clerick <devnextdoorcoders@gmail.com>
2022-04-25 21:34:21 +0300

    change save actions for timetable +

--- resources/views/user/timetable.blade.php
+++ resources/views/user/timetable.blade.php
@@ -22,12 +22,19 @@
         {{--        @endslot--}}
 
         @include('layouts.timetable')
+        @slot('header_buttons')
+            @if($mode == 'edit')
+                <button type="button" id="time-confirm" class="btn-primary">
+                    {{ __('Подтвердить') }}
+                </button>
+            @endif
+        @endslot
 
         @slot('buttons')
             @if($mode == 'edit')
-            <button type="button" id="time-confirm" data-id="{{$currentService}}" class="btn-primary">
-                {{ __('Подтвердить') }}
-            </button>
+{{--            <button type="button" id="time-confirm" data-id="{{$currentService}}" class="btn-primary">--}}
+{{--                {{ __('Подтвердить') }}--}}
+{{--            </button>--}}
             @endif
             @if ($id)
                 <a href="{{route('addService', ['business' => $slug, 'modal' => $mode, 'id' => $id, 'moreService' => ($mode == 'edit' ? $moreService : null)])}}"
